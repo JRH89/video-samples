a5718104a577dc64e723bcc7af5fa9c6cc1ff6d7
JRH89 <thegemlins@gmail.com>
2023-10-25 14:24:58 -0700

    from 4k to HD

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ const Video = ({ src }) => {
 const Home = () => {
   const videos = [
 
-    "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-24_3.mp4?alt=media&token=5015cbce-af2a-4bc2-905e-8e285d964d6f",
+    "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-24_3.mp4?alt=media&token=baeb9dbf-8849-4f53-8365-cd3f94fd74f4",
     "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-7_3_HD_3.mp4?alt=media&token=dd5f88d1-a82f-4e2d-a5ee-e70347247d7e",
     "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-13_HD_8.mp4?alt=media&token=e940a3c1-2136-4dce-a54f-1498a0229381"
 
@@ -37,7 +37,7 @@ const Home = () => {
     <>
       <div className="bg-gray-100 min-h-screen p-6 my-auto place-content-center mx-auto flex flex-col">
         <div className="max-w-screen-lg mx-auto">
-          <h1 className="text-2xl underline font-bold mb-4 text-center text-neutral-950">Video Logo Samples</h1>
+          <h1 className="text-2xl underline font-bold mb-4 text-center text-neutral-950">Animated 3D Logo Samples</h1>
           <div className="space-y-4">
             {videos.map((video, index) => (
               <Video key={index} src={video} />
@@ -47,7 +47,7 @@ const Home = () => {
       </div>
       <div className="bg-gray-100 min-h-screen p-6 my-auto place-content-center mx-auto flex flex-col">
         <div className="max-w-screen-lg mx-auto">
-          <h1 className="text-2xl underline font-bold mb-4 text-center text-neutral-950">Image Samples</h1>
+          <h1 className="text-2xl underline font-bold mb-4 text-center text-neutral-950">3D Logo Samples</h1>
           <div className="space-y-4">
             {images.map((image, index) => (
               <Image key={index} src={image} height={1080} alt="Images" width={1920} />
