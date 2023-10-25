2171d3cc2fdd704c0bbf812e322df354cd9c31b4
JRH89 <thegemlins@gmail.com>
2023-10-24 23:16:07 -0700

    images/videos

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Video Logo Samples',
+  title: 'Logo Samples',
   description: 'Hooker Hill Studios',
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Image from "next/image"
 
 const Video = ({ src }) => {
   return (
@@ -16,9 +17,20 @@ const Video = ({ src }) => {
 
 const Home = () => {
   const videos = [
-    "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-13_HD_8.mp4?alt=media&token=e940a3c1-2136-4dce-a54f-1498a0229381",
+
+    "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-24_3.mp4?alt=media&token=5015cbce-af2a-4bc2-905e-8e285d964d6f",
     "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-7_3_HD_3.mp4?alt=media&token=dd5f88d1-a82f-4e2d-a5ee-e70347247d7e",
-    "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-24_2_4K.mp4?alt=media&token=b1ab3525-044b-4356-a9fc-529fd4dd0af9",
+    "https://firebasestorage.googleapis.com/v0/b/builders-land.appspot.com/o/videos%2F10-13_HD_8.mp4?alt=media&token=e940a3c1-2136-4dce-a54f-1498a0229381"
+
+  ]
+  const images = [
+    "/003.png",
+    "/004.png",
+    "/005.png",
+    "/001.png",
+    "/002.png",
+    "/006.png",
+    "/007.png",
   ]
 
   return (
@@ -28,25 +40,36 @@ const Home = () => {
           <h1 className="text-2xl underline font-bold mb-4 text-center text-neutral-950">Video Logo Samples</h1>
           <div className="space-y-4">
             {videos.map((video, index) => (
-              <Video
-                key={index} src={video} />
+              <Video key={index} src={video} />
             ))}
           </div>
         </div>
-
       </div>
-      <div
-        className="bg-neutral-950 text-center duration-200"
-      >
+      <div className="bg-gray-100 min-h-screen p-6 my-auto place-content-center mx-auto flex flex-col">
+        <div className="max-w-screen-lg mx-auto">
+          <h1 className="text-2xl underline font-bold mb-4 text-center text-neutral-950">Image Samples</h1>
+          <div className="space-y-4">
+            {images.map((image, index) => (
+              <Image key={index} src={image} height={1080} alt="Images" width={1920} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="bg-neutral-950 text-center duration-200">
         <a href="https://www.hookerhillstudios.com" class="hover-link transition duration-300">
           <span class="word">Hooker </span>
           <span class="word">Hill </span>
           <span class="word">Studios</span>
         </a>
-
       </div>
     </>
   )
 }
 
-export default Home
+export default Home;
+
+
+
+
+
+
